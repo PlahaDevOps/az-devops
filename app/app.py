b89994c352687a31e663ced2d4c187465a5154f9
Ubuntu <azureuser@devops-vm1.t5oxakpvod5ujomm35hb53btfe.bx.internal.cloudapp.net>
2025-05-30 14:39:10 +0000

    Inject SECRET_TOKEN from Key Vault into Flaks app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
     return "🚀 Hello from Flask inside Docker on Azure VM!"
 
+@app.route("/secret")
+def show_secret():
+    secret = os.getenv("SECRET_TOKEN", "Secret not set")
+    return f"Secret Token: {secret}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
